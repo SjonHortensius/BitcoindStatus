c2670d816133bd01d43b2b2fed23f9b6a8b09a15
Sjon Hortensius <sjon@hortensius.net>
2017-03-11 18:10:29 +0100

    move submodules, add peers with geoip support, update style

--- index.php
+++ index.php
@@ -7,9 +7,78 @@ spl_autoload_register(function($class){
 	$class = str_replace('\\', '/', $class);
 
 	if (0 === strpos($class, 'TooBasic/Rpc/'))
-		require(__DIR__ .'/library/TooBasic-Rpc/'. substr($class, strlen('TooBasic/Rpc/')) .'.php');
+		require('TooBasic-Rpc/'. substr($class, strlen('TooBasic/Rpc/')) .'.php');
+	elseif (0 === strpos($class, 'MaxMind/'))
+		require('MaxMind/src/MaxMind/Db/'. substr($class, strlen('MaxMind/Db/')) .'.php');
 	else
-		require(__DIR__ .'/library/'. $class .'.php');
+		require($class .'.php');
 });
 
-BitcoindStatus::dispatch(substr($_SERVER['REQUEST_URI'], strlen('/'.__DIR__)));
+class BitcoindStatus extends TooBasic\Controller
+{
+	protected $_config;
+	public static $client;
+	public static $tpl;
+
+	protected function _construct()
+	{
+		require('config.php');
+		$this->_config = $config;
+
+		self::$tpl = new TooBasic\Template;
+
+		$curl = new TooBasic\Rpc\Transport\Curl;
+		$curl->setOption(CURLOPT_USERPWD, $config['rpcUser'].':'.$config['rpcPassword']);
+		self::$client = new TooBasic\Rpc\Client\Json('http://'.$config['rpcHost'].'/', $curl);
+
+		self::$tpl->geoIp = new MaxMind\Db\Reader(__DIR__.'/GeoLite2-Country.mmdb');
+	}
+
+	protected function _handle(TooBasic\Exception $e)
+	{
+		if (!headers_sent())
+		{
+			header('Content-Type: text/plain');
+			http_response_code(500);
+		}
+
+		echo str_replace(dirname(__DIR__), '.', $e);
+	}
+
+	public function getIndex()
+	{
+		self::$tpl->info = self::$client->getinfo();
+		self::$tpl->info->network = self::$client->getnetworkinfo();
+		self::$tpl->info->mempool = self::$client->getmempoolinfo();
+
+		$curl = new TooBasic\Rpc\Transport\Curl;
+		$network = json_decode($curl->request('GET', 'https://bitnodes.21.co/api/v1/snapshots/'));
+		self::$tpl->blockCount = $network->results[0]->latest_height;
+
+		if (isset($this->_config['publicHost']))
+			self::$tpl->onlineInfo = json_decode($curl->request('GET', 'https://bitnodes.21.co/api/v1/nodes/'.strtr($this->_config['publicHost'].'/', ':', '-')));
+		else
+			self::$tpl->onlineInfo = (object)['status' => '<i>unknown</i>'];
+
+		self::$tpl->peers = self::$client->getpeerinfo();
+		usort(self::$tpl->peers, function($a, $b){
+			return intval(100000*($a->minping - $b->minping));
+		});
+
+
+		print self::$tpl->get('index')->getWrapped();
+	}
+
+	public static function binaryPrefix(int $size, $precision = 0, $format = '%.2f %sb')
+	{
+		$prefixes = ['K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+		$prefix = '';
+
+		while ($size >= 1024 && $prefix = array_shift($prefixes))
+			$size = round($size/1024, $precision);
+
+		return sprintf($format, $size, $prefix);
+	}
+}
+
+BitcoindStatus::dispatch('/'. $_SERVER['QUERY_STRING']);
--- tpl/index.php
+++ tpl/index.php
@@ -1,34 +1,59 @@
-<div class="row marketing">
-	<div class="col-lg-6">
-		<h4>Node</h4>
-		<div class="table-responsive">
-			<table class="table table-striped">
-				<tr><th>Node software</th><td><?=$this->info->network->subversion?></td></tr>
-				<tr><th>Bitcoin / Protocol version</th><td><?=$this->info->version?> / <?=$this->info->protocolversion?></td></tr>
-				<tr><th>Block height</th><td><?=$this->info->blocks?> [<?=sprintf('%.2d%%', (100/$this->blockCount)*$this->info->blocks)?>]</td></tr>
-				<tr><th>Uptime</th><td>xx</td></tr>
-				<tr><th>Disk usage</th><td>xx (xx free)</td></tr>
-				<tr><th>Memory usage</th><td>xx (xx free)</td></tr>
-			</table>
-		</div>
+<div class="col-lg-12">
+	<h4>Node</h4>
+	<div class="table-responsive">
+		<table class="table table-striped">
+			<tr><th>Node software</th><td><?=$this->info->network->subversion?></td></tr>
+			<tr><th>Bitcoin / Protocol version</th><td><?=$this->info->version?> / <?=$this->info->protocolversion?></td></tr>
+			<tr><th>Block height</th><td><?=$this->info->blocks?> [<?=sprintf('%01.2f%%', (100/$this->blockCount)*$this->info->blocks)?>]</td></tr>
+			<tr><th>Uptime</th><td>xx</td></tr>
+			<tr><th>Disk usage</th><td>xx (xx free)</td></tr>
+			<tr><th>Memory usage</th><td>xx (xx free)</td></tr>
+		</table>
+	</div>
 
-		<h4>Network</h4>
-		<div class="table-responsive">
-			<table class="table table-striped">
-				<tr><th>Online status (via <a href="https://21.co/">21.co</a>)</th><td><?=$this->onlineInfo->status?></td></tr>
-				<tr><th>Connections</th><td><?=$this->info->connections?></td></tr>
-				<?foreach ($this->info->network->networks as $network):?>
-					<tr><th><?=$network->name?></th><td><?=$network->reachable?'true':'false'?></td></tr>
-				<?endforeach?>
-			</table>
-		</div>
+	<h4>Network</h4>
+	<div class="table-responsive">
+		<table class="table table-striped">
+			<tr><th>Online status (via <a href="https://21.co/">21.co</a>)</th><td><?=$this->onlineInfo->status?></td></tr>
+			<tr><th>Connections</th><td><?=$this->info->connections?></td></tr>
+			<?foreach ($this->info->network->networks as $network):?>
+				<tr><th><?=$network->name?></th><td><?=$network->reachable?'true':'false'?></td></tr>
+			<?endforeach?>
+		</table>
+	</div>
 
-		<h4>Memory pool</h4>
-		<div class="table-responsive">
-			<table class="table table-striped">
-				<tr><th># Transactions</th><td><?=$this->info->mempool->size?></td></tr>
-				<tr><th>Size</th><td><?=BitcoindStatus::binaryPrefix($this->info->mempool->usage)?> (max <?=BitcoindStatus::binaryPrefix($this->info->mempool->maxmempool)?>)</td></tr>
-			</table>
-		</div>
+	<h4>Memory pool</h4>
+	<div class="table-responsive">
+		<table class="table table-striped">
+			<tr><th># Transactions</th><td><?=$this->info->mempool->size?></td></tr>
+			<tr><th>Size</th><td><?=BitcoindStatus::binaryPrefix($this->info->mempool->usage)?> (max <?=BitcoindStatus::binaryPrefix($this->info->mempool->maxmempool)?>)</td></tr>
+		</table>
+	</div>
+
+	<h4>Peers</h4>
+	<div class="table-responsive">
+		<table class="table table-striped" id="peers">
+			<thead>
+				<tr>
+					<th>Country</th>
+					<th>Address</th>
+					<th>Ping</th>
+					<th>Height</th>
+					<th>Version</th>
+					<th>Bytes send/received</th>
+				</tr>
+			</thead>
+			<?foreach($this->peers as $peer):?>
+			<?$geo = $this->geoIp->get(substr($peer->addr, 0, strrpos($peer->addr, ':')))?>
+				<tr>
+					<td title="<?=$geo['country']['names']['en']?>"><?=$geo['country']['iso_code']?></td>
+					<td><?=$peer->addr?></td>
+					<td><?=round(100*$peer->minping)?> (+/- <?=100*($peer->pingtime-$peer->minping)?>)</td>
+					<td><?=$peer->synced_blocks-$this->info->blocks?></td>
+					<td><?=trim($peer->subver, '/')?></td>
+					<td><?=BitcoindStatus::binaryPrefix($peer->bytessent)?>/<?=BitcoindStatus::binaryPrefix($peer->bytesrecv)?></td>
+				</tr>
+			<?endforeach;?>
+		</table>
 	</div>
 </div>
